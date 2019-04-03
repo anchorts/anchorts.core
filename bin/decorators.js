47908533cf6f99986c2ff2bba41a0070e208e019
Tinh Ngo <tinh.ngo@niteco.se>
2019-04-03 13:52:31 +0700

    update decorators

--- bin/decorators.js
+++ bin/decorators.js
@@ -1,21 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.provider = new Map();
-exports.INJECTION_KEY = 'INJECTION_KEY';
+exports.INJECTION_TOKEN_KEY = 'INJECTION_TOKEN_KEY';
+exports.PARAMS_KEY = 'design:paramtypes';
 function inject(token) {
     return function (target, propertyKey, parameterIndex) {
         // console.log(token);
         // console.log(target);
         // console.log(propertyKey);
         // console.log(parameterIndex);
-        Reflect.defineMetadata(exports.INJECTION_KEY, token, target);
+        let injectionTokens = Reflect.getOwnMetadata(exports.INJECTION_TOKEN_KEY, target) || {};
+        injectionTokens[parameterIndex] = token;
+        Reflect.defineMetadata(exports.INJECTION_TOKEN_KEY, injectionTokens, target);
     };
 }
 exports.inject = inject;
 function injectable() {
     return function (target) {
-        console.log(target);
-        // provider.set(target, );
+        let params = Reflect.getOwnMetadata(exports.PARAMS_KEY, target) || [];
+        let injectionTokens = Reflect.getOwnMetadata(exports.INJECTION_TOKEN_KEY, target) || {};
+        console.log(injectionTokens);
+        console.log(params);
     };
 }
 exports.injectable = injectable;
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,27 +1,33 @@
-export type constructor<T> = {new (...args: any[]): T};
+export type constructor<T> = { new(...args: any[]): T };
 
 export type InjectionToken<T> = string | symbol | constructor<T>;
 
-export type Dictionary<T> = {[key: string] : T};
+export type Dictionary<T> = { [key: string]: T };
 
-export const provider = new Map<constructor<any>, any[]> ();
+export const provider = new Map<constructor<any>, any[]>();
 
 export const INJECTION_TOKEN_KEY = 'INJECTION_TOKEN_KEY';
 
+export const PARAMS_KEY = 'design:paramtypes';
+
 export function inject<T>(token: InjectionToken<T>) {
-    return function(target: Object, propertyKey: string, parameterIndex: number) {
+    return function (target: Object, propertyKey: string, parameterIndex: number) {
         // console.log(token);
         // console.log(target);
         // console.log(propertyKey);
         // console.log(parameterIndex);
-
-        Reflect.defineMetadata(INJECTION_TOKEN_KEY, token, target);
+        let injectionTokens = Reflect.getOwnMetadata(INJECTION_TOKEN_KEY, target) || {};
+        injectionTokens[parameterIndex] = token;
+        Reflect.defineMetadata(INJECTION_TOKEN_KEY, injectionTokens, target);
     }
 }
 
 export function injectable<T>() {
-    return function(target: constructor<T>) {
-        console.log(target);
+    return function (target: constructor<T>) {
+        let params: any[] = Reflect.getOwnMetadata(PARAMS_KEY, target) || [];
+        let injectionTokens = Reflect.getOwnMetadata(INJECTION_TOKEN_KEY, target) || {};
+        console.log(injectionTokens);
+        console.log(params);
     }
 }
 
